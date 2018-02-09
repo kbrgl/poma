ac1dd97b5ae90e5c87aa49b75ac44520fcf7e3e6
Kabir Goel <kabirgoel.kg@gmail.com>
2018-02-09 21:12:21 +0530

    Add flow and test it on utils/time.js

--- utils/time.js
+++ utils/time.js
@@ -1,15 +1,17 @@
+// @flow
+
 // Pad zeroes for formatting time so 6m5s shows up as 6:05 rather than 6:5
 const padTimeZeroes = t => (t % 60 < 10 ? "0" : "") + t % 60
 
 // Format given number of ms as minutes:seconds
-export const formatMS = ms => {
+export const formatMS = (ms: number) => {
   let time = Math.ceil(ms / 1000)
-  time = padTimeZeroes(Math.trunc(time / 60)) + ":" + padTimeZeroes(time)
+  time = `${padTimeZeroes(Math.trunc(time / 60))}:${padTimeZeroes(time)}`
   return time
 }
 
 // Time units to standard milliseconds
-export const milliseconds = n => n
-export const seconds = n => n * milliseconds(1000)
-export const minutes = n => n * seconds(60)
-export const hours = n => n * minutes(60)
+export const milliseconds = (n: number) => n
+export const seconds = (n: number) => n * milliseconds(1000)
+export const minutes = (n: number) => n * seconds(60)
+export const hours = (n: number) => n * minutes(60)
